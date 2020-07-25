1181a9d0ee2aa471bef088f7c166055ed1744fe4
korisu07 <Hallo.Dreamy@gmail.com>
2020-07-25 14:51:33 +0900

    省略のための処理を作成。

--- public/js/omitContent.js
+++ public/js/omitContent.js
@@ -1,44 +1,36 @@
-
+const MaxLength = 50; //表示する文字数
 
 // 50文字以下の場合省略する
-function omitContent(string) {
-  const MaxLength = 50; //表示する文字数
-
-  // 50文字を超えた場合、超えた分を省略する処理
-  if (string.length > MaxLength) {
-    return string.substr(0, MaxLength) + '...';
-  }
-  // 50文字以内ならそのまま表示
-  return string;
-}
-
-function sizeControl(string) {
+function omitPreContent(string) {
   const
     //div.memoの高さを取得
-    memoElement = window.document.getElementById('memoBox'),
-    divMemoSize = memoElement.scrollHeight;
-
-  console.log(divMemoSize);
+    memoElement = document.getElementById('memoBox'),
+    divMemoSize = memoElement.clientHeight;
 
   const preHeight = string.clientHeight;
+  const preLength = string.textContent.length;
 
-  // preタグの高さがdiv.memoを超えたら発動
-  if (preHeight - 20 > 200) {
-    return string.textContent.substr(0, 3) + '...';
+  // 50文字を超えた場合、超えた分を省略する処理
+  if (string.textContent.length > MaxLength) {
+    return string.textContent.substr(0, MaxLength) + '...';
+  }
+
+  // 50文字以内でも、preタグの高さがdiv.memoを超えたら発動
+  else if (preHeight > divMemoSize && preLength <= MaxLength) {
+    return string.textContent = string.textContent.substr(0, 3) + '...';
+  }
+
+  // 50文字以内でpreがはみ出していない場合は、そのまま表示
+  else {
+    return string.textContent;
   }
 }
 
 // preタグをすべて読み込み
-const contents = window.document.querySelectorAll('pre');
-
-// preタグを一つずつ処理
-contents.forEach(pre => {
-  sizeControl(pre);
-  console.log(pre.clientHeight - 20);
-});
+const contents = document.querySelectorAll('pre');
 
 // 関数を実行するための処理
 // preタグを一つずつ処理
 contents.forEach(content => {
-  content.textContent = omitContent(content.textContent);
+  content.textContent = omitPreContent(content);
 });
