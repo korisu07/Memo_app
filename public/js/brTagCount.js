73a195fea8d1866f4f157d44cbacfe08b03703ae
korisu07 <Hallo.Dreamy@gmail.com>
2020-07-27 16:30:10 +0900

    省略についての条件について作成中。正規表現の修正が必要。

--- public/js/brTagCount.js
+++ public/js/brTagCount.js
@@ -1,7 +1,28 @@
-function brTagCount(Element) {
-  const brTag = '\n|\r\n|\r';
+function brTagSearch(Element) {
+  const str = '(\n|\r\n|\r){5}';
 
-  const count = (Element.match(new RegExp(brTag, "g")) || []).length;
-
-  return count;
+  return Element.replace((new RegExp(str, 'g')), 'test');
 }
+
+// function brTagChange(Element) {
+//   const br = '^([^<br>]+<br>){5}';
+
+//   return Element.replace((new RegExp(br)), '...');
+// }
+
+// preタグをすべて読み込み
+const contents = document.querySelectorAll('pre');
+
+// brTagSearch(contents);
+
+// 関数を実行するための処理
+// preタグを一つずつ処理
+contents.forEach(pre => {
+  pre.innerHTML = brTagSearch(pre.innerHTML);
+});
+
+
+//惜しい所までいけた。
+//５番目の改行を&nbsp;に変換することができるなら、突破口が見える。
+//最終的に、&nbsp;までの文字数をそれぞれカウントさせて、
+//その文字数までを切り出したい。
