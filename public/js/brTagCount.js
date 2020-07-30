47085f5e3037d448630b26b325df43116f19c3eb
korisu07 <Hallo.Dreamy@gmail.com>
2020-07-30 14:53:50 +0900

    ID名：memoBoxの削除に伴い関数を改善。関数名変更アリ。検索条件は途中。

--- public/js/brTagCount.js
+++ public/js/brTagCount.js
@@ -1,15 +1,14 @@
 // 改行をnbspに変換する
-function brTagSearch(Element) {
+function brTagChange(Element) {
   const searchStr = '\n|\r\n|\r';
-  return Element.replace((new RegExp(searchStr, 'm')), '&nbsp;');
+  return Element.replace((new RegExp(searchStr, 'g')), '<br>');
 }
 
 // nbspまでの文字列を切り出し
 function brTagString(Element) {
   const str = '&nbsp;';
 
-  const index = str.indexOf(Element, 5);
-
+  const index = str.indexOf(Element);
   return Element.substring(0, index) + '...';
 
 }
@@ -21,15 +20,15 @@ function brTagString(Element) {
 // }
 
 // preタグをすべて読み込み
-const contents = document.querySelectorAll('pre');
+const contents = document.querySelectorAll('div.memo label');
 
 // brTagSearch(contents);
 
 // 関数を実行するための処理
 // preタグを一つずつ処理
-contents.forEach(pre => {
-  pre.innerHTML = brTagSearch(pre.innerHTML);
-  pre.textContent = brTagString(pre.textContent);
+contents.forEach(box => {
+  box.innerHTML = brTagChange(box.innerHTML);
+  // pre.textContent = brTagString(pre.textContent);
 });
 
 
