68687511a2c9ee72ac6d24fe53e01646fb47f63c
korisu07 <Hallo.Dreamy@gmail.com>
2020-08-01 17:27:34 +0900

    処理は作れた。改善の余地あり。

--- public/js/omitContent.js
+++ public/js/omitContent.js
@@ -1,7 +1,15 @@
-// 改行をnbspに変換する
+// 改行をbrタグに変換する
 function brTagChange(Element) {
-  const searchStr = '\n|\r\n|\r';
-  return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  const searchStr = '\n|\r\n|\r',
+    //改行があるかどうかを探すmatch関数
+    matchTag = Element.match(new RegExp(searchStr, 'g'));
+
+  // 後ろにある★のmatch関数で配列を作り出すために一時的に改行タグを入れます。
+  if (matchTag == null) {
+    return Element + '<br><br><br><br><br>';
+  } else {
+    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  }
 }
 
 // nbspまでの文字列を切り出し
@@ -18,26 +26,25 @@ function brTagString(Element) {
   const
     textHeight = Element.clientHeight;
 
+  //★改行が５回されたらマッチして、５回めの改行を配列として切り出す
   const match = Element.innerHTML.match(new RegExp('(<br>|[^<br>]+<br>){5}'));
-  // console.log(match);
-
-  const index = Element.innerHTML.lastIndexOf(match[1]);
-  // console.log(index);
-
-  const MaxLength = 30; //表示する文字数
-
-  // textの高さがdiv.memoを超えたら発動
-  if (textHeight >= divMemoSize - 25) {
+  console.log(match);
+  const obj = match[0];
+  // match[0]の配列が作られて、かつtextの高さがdiv.memoを超えたら発動
+  if (obj && textHeight >= divMemoSize - 25 && Element.textContent.length <= 50) {
+    const index = match[0].length;
+    console.log(index);
     return Element.innerHTML.substr(0, index) + '...';
   }
-  else if (Element.textContent.length >= MaxLength) {
-    return Element.innerHTML.substr(0, MaxLength) + '...';
+  // 50文字を超えている場合に発動します。
+  else if (Element.textContent.length >= 50) {
+    return Element.innerHTML.substr(0, 50) + '...';
   }
+  // 50文字を超えていない場合、そのまま返します。
   else {
     return Element.innerHTML;
   }
 }
-
 
 
 // preタグをすべて読み込み
