8780f0f539d1350b9e2830ab781162cc60b0ffb9
korisu07 <Hallo.Dreamy@gmail.com>
2020-08-04 19:29:55 +0900

    表示したメモの内容を読みやすいように文字列を変換する関数を作成。

--- public/js/memoContentView.js
+++ public/js/memoContentView.js
@@ -1,12 +1,39 @@
-// メモの内容表示のときにつくJSON形式の感じを変換したい
+// メモの内容表示のときにつくJSON形式のカッコ等を変換したい
 
+//前提：pugファイルに表示するために、
+//メモの内容がJSON形式で表示されています。
+//本当は直接変換したかったけど無理でした
+
+//メモのdivを読み込み
 const
-  textJSON = document.getElementById('memoText');
+  loadMemoContent = document.querySelector('div#memoText');
 
-console.log(textJSON);
+console.log(loadMemoContent.textContent);
 
-function str(elem) {
-  return elem.replace(new RegExp('\n|\r\n|\r', 'g'), '<br>');
+//JSON形式になっているテキストを
+//JavaScriptで扱えるように変換
+const
+  textOutPut = JSON.parse(loadMemoContent.textContent),
+  //配列として取り出された最初のもの（これがメモの内容）の値を取得
+  textValue = Object.values(textOutPut[0]);
+
+console.log(textValue);
+
+//再度JSON形式として変換するときにまわりのカギカッコ等を削除
+const
+  content =
+    JSON.stringify(textValue)
+      .replace(/(")|(.$)|(^.)|(\\r|\\r\\n|\\n)/g, function () {
+        if (arguments[4]) {
+          return '<br>';
+        } else if (arguments[0]) {
+          return '';
+        }
+      });
+function strChange(elem) {
+  return elem;
 }
 
-textJSON.innerHTML = str(textJSON.textContent);
+console.log(content);
+
+loadMemoContent.innerHTML = strChange(content);
