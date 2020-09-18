0577b838c9a52e708805ab93e6d72ae35c74f0b2
korisu07 <Hallo.Dreamy@gmail.com>
2020-09-18 14:07:36 +0900

    内容表示についての処理を更新。また、初期メモがある場合の条件分岐を追加。

--- public/js/clickOpenAction.js
+++ public/js/clickOpenAction.js
@@ -9,17 +9,47 @@ const
   //半透明の黒背景
   overLay = document.getElementById('overLay'),
   //表示するメモの中身
-  memoView = document.getElementById('memoView');
+  memoView = document.getElementById('memoView'),
+  memoText = document.getElementById('memoText');
 
+  //表示部分のボタンを読み込み
+  const 
+  editBtn = document.getElementById('editMemo'),
+  deleteBtn = document.getElementById('deleteMemo');
 
   //メモを表示する
 　//表示ボタンをひとつずつ処理
   arrayBtn.forEach(btn => {
   //「メモを表示する」ボタンをクリックしたときに発動
   btn.addEventListener('click', function () {
-    
-    //テキストの中身を表示
+  
+    //テキストの中身を表示する処理
+    const
+      //クリックされたボタンのid名から数字だけを取得
+      idNumber = btn.id.replace('open_', ''),
+      //cookieから対応する番号の内容を読み込み
+      cookieContent = decodeURI(document.cookie);
 
+      //初期のメモが表示されている場合
+      if(btn.id === 'open_default'){
+        memoText.innerHTML = document.getElementById('content_default').innerHTML;
+      }
+      //ユーザーによるメモが登録されている場合
+      else{
+        const 
+        //「;」で分割し配列に
+          cookiesArray = cookieContent.split(';'),
+        //idNumberと一致する配列を参照。
+        //「=」までの文字数をカウント。
+          index = cookiesArray[idNumber].indexOf('=') + 1;
+        //「=」より後ろの文字を表示させる
+          memoText.innerHTML = cookiesArray[idNumber].substr(index);
+      }
+
+      editBtn.classList.add('edit_' + String(idNumber));
+      deleteBtn.classList.add('delete_' + String(idNumber));
+
+    //表示に関する処理
     //それぞれの要素を表示
     overLay.style.display = 'block';
     memoView.style.display = 'block';
@@ -46,6 +76,9 @@ function closeMemoContent(elem){
   elem.addEventListener('click', function () {
      //重複防止のため、pointerEventsを一時的に無効化
     pointerEventsChange('none');
+
+    editBtn.classList.remove();
+    deleteBtn.classList.remove();
 
     //フェードアウトの処理
     overLay.animate([{ opacity: '0.5' }, { opacity: '0' }], 220);
