609dd0170814b5b27a0b2e57dd5a475931a093b5
korisu07 <Hallo.Dreamy@gmail.com>
2020-10-05 02:38:09 +0900

    修正

--- app.js
+++ app.js
@@ -149,6 +149,6 @@ app.post('/edit/post/:id', (req, res) => {
 });
 
 // Localhost:3000に接続
-app.listen(3000, function () {
+app.listen(3000, '124.154.136.111', function () {
   console.log('Successful! App listening on port 3000.');
 });
