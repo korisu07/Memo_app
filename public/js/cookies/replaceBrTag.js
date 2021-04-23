c6dce9d7ce99319b812a931326c8bdd8a88c097c
korisu07 <Hallo.Dreamy@gmail.com>
2021-04-23 15:00:49 +0900

    add export setting, add new comment.

--- public/js/cookies/replaceBrTag.js
+++ public/js/cookies/replaceBrTag.js
@@ -1,18 +1,8 @@
 'use strict';
 
-// 改行をbrタグに変換する
-function brTagChange(Element) {
-  let searchStr = /(\r\n)/;
-  const searchExp = Element.match(new RegExp(searchStr, 'g'));
+export { brTagChange, escapeHTMLtags };
 
-  if(searchExp){
-    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
-  }else{
-    searchStr = /(\r)|(\n)/;
-    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
-  }
-}
-
+// HTMLタグを除外する処理
 function escapeHTMLtags( target ){
   // HTMLタグを検索し、除外
   target = target.replace(/<[a-z]*>/g, '');
@@ -20,4 +10,21 @@ function escapeHTMLtags( target ){
 
   // 除外したあとの文字列を返す
   return target;
+}
+
+// 改行コードを <br>タグ に変換する処理
+function brTagChange(Element) {
+  // はじめに、\r\n形式の改行コードを検索します。
+  let searchStr = /(\r\n)/;
+  const searchExp = Element.match(new RegExp(searchStr, 'g'));
+
+  // 一致した場合、改行コードを <br>タグ へ変換
+  if(searchExp){
+    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  }else{
+    // 一致しなかった場合、 \r もしくは \n を改行コードとして検索
+    // そちらを <br>タグ へ変換
+    searchStr = /(\r)|(\n)/;
+    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  }
 }
