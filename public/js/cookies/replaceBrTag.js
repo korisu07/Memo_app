0c492f58878e51d72aa55f4b3f4fccd8161a2e58
korisu07 <Hallo.Dreamy@gmail.com>
2021-04-23 13:33:11 +0900

    copy function from clickOpenAction.js

--- public/js/cookies/replaceBrTag.js
+++ public/js/cookies/replaceBrTag.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// 改行をbrタグに変換する
+function brTagChange(Element) {
+  let searchStr = /(\r\n)/;
+  const searchExp = Element.match(new RegExp(searchStr, 'g'));
+
+  if(searchExp){
+    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  }else{
+    searchStr = /(\r)|(\n)/;
+    return Element.replace(new RegExp(searchStr, 'g'), '<br>');
+  }
+}
