7f556f8d4d4d516785e932ee5fc72b0ae1a446da
korisu07 <Hallo.Dreamy@gmail.com>
2021-04-26 18:14:52 +0900

    split function. add  two function.

--- public/js/omitContent.js
+++ public/js/omitContent.js
@@ -2,25 +2,48 @@
 
 import { brTagChange, escapeHTMLtags } from "./cookies/replaceBrTag.js";
 
+
+// 改行チェック用の条件
+// 改行が５回されたらマッチして、５回目の改行を配列として切り出す
+function match5Br(targetText) {
+  return targetText.match(new RegExp('(<br>|[^<br>]+<br>){4}'))
+}
+ 
+// -----------------------------------------------
+
+// 改行が5回以上繰り返されているかをチェックする
+function boolsearch5Br( targetText ){
+  let resultOfSearch;
+
+  // 改行が5回以上繰り返されている場合、true判定を返す
+  if( match5Br(targetText) ){
+    resultOfSearch = true;
+  } else {
+    resultOfSearch = false;
+  }
+
+  return resultOfSearch;
+}
+
+// -----------------------------------------------
+
 // 文字省略のための処理
-function brTagString(Element) {
+function brTagString(Element, bool) {
 
   //div.memoの高さを取得
   const
-    memoElement = document.querySelector('div.memo'),
-    divMemoSize = memoElement.clientHeight;
+    divMemoSize = document.querySelector('div.memo').clientHeight,
 
   //textの高さと文字数を取得
-  const
     h3Size = document.querySelector('h3').clientHeight,
-    textHeight = Element.clientHeight + h3Size - 15,
+    textHeight = Element.clientHeight + h3Size - 15;
 
-  //★改行が５回されたらマッチして、５回目の改行を配列として切り出す
-    match = Element.innerHTML.match(new RegExp('(<br>|[^<br>]+<br>){4}'));
+  let 
+    targetBr = match5Br(Element.innerHTML);
 
-  // matchが作られて、かつtextの高さがdiv.memoを超えたら発動(50文字未満)
-  if (match && textHeight >= divMemoSize - 25 && Element.textContent.length <= 50) {
-    const index = match[0].length;
+  // textの高さがdiv.memoを超えたら発動(50文字未満)
+  if( bool && textHeight >= divMemoSize - 25 && Element.textContent.length <= 50){
+    const index = targetBr[0].length;
     return Element.innerHTML.substr(0, index) + '...';
   }
   // 50文字を超えている場合に発動
@@ -41,7 +64,9 @@ const contents = document.querySelectorAll('div.smallMemoContent');
 contents.forEach(memoBox => {
   const 
     content = escapeHTMLtags(memoBox.innerHTML);
-    
+  
+  // 改行コードを <br>タグ に変換
   memoBox.innerHTML = brTagChange(content);
-  memoBox.innerHTML = brTagString(memoBox);
+  // 変換済みのメモ内のテキストで、条件に合致したものの内容を省略
+  memoBox.innerHTML = brTagString(memoBox, boolsearch5Br(memoBox.innerHTML) );
 });
