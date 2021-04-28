dad86dcce1eba6540bed81b08dcbacdb7e9647b1
korisu07 <Hallo.Dreamy@gmail.com>
2021-04-28 17:55:04 +0900

    update test function. If click, add test-div.

--- public/js/action/clickOpenModal.js
+++ public/js/action/clickOpenModal.js
@@ -28,32 +28,50 @@ const
     </div>
   `;
 
-// 
-// ここまで　グローバルスコープ　
-// 
+const
+  testDiv = '<div id="addTestDiv">This is test!<div>'
 
-// -----------------------------------------------
-
-function addModal( action ){
-  action.addEventListener('click', function(){
-    let wrapper;
   
-    wrapper = document.getElementsByClassName('memoWrap');
-    wrapper = Array.from(wrapper)[0];
-
-    wrapper.insertAdjacentHTML('afterend',contentOfModal);
-
-    console.log('add modal.');
-  });
-}
-
 let
 　//表示ボタンをすべて読み込み
   arrayMemoBtn = document.getElementsByClassName('test');
   //クラスを配列化
   arrayMemoBtn = Array.from(arrayMemoBtn);
 
+// 
+// ここまで　グローバルスコープ　
+// 
 
+// -----------------------------------------------
+
+function addModal( target ){
+    let wrapper;
+  
+    wrapper = document.getElementsByClassName('memoWrap');
+    wrapper = Array.from(wrapper)[0];
+
+    wrapper.insertAdjacentHTML('afterend', target);
+
+    console.log('add modal.');
+}
+
+function deleteModal( target ){
+  target.remove();
+  console.log('delete modal.');
+}
+
+
+// クリック時に発動
+// ※何故かgetElementByIdを定数にすると上手く動きません
 arrayMemoBtn.forEach(btn => {
-  addModal( btn );
+  btn.addEventListener('click', function(){
+    // Modalが存在しない場合
+    if(document.getElementById('addTestDiv') === null){
+      // Modalを追加
+      addModal( testDiv );
+    } else { //存在する場合
+      // Modalを削除
+      deleteModal( document.getElementById('addTestDiv') );
+    }
+  });
 });
