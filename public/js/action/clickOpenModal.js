acb77641a4a0635b542c7c6828e1a4ecbf93984f
korisu07 <Hallo.Dreamy@gmail.com>
2021-04-28 15:58:09 +0900

    add Model.

--- public/js/action/clickOpenModal.js
+++ public/js/action/clickOpenModal.js
@@ -1,1 +1,35 @@
 'use strict';
+
+
+// 
+// ★グローバルスコープ
+// 
+
+const
+  contentOfModal = `
+    <div id="modalWrapp">
+      <div id="overLay"></div>
+      <div id="modalMemoWindow">
+        <h2></h2>
+
+        <div class="wrapMenu">
+          <span id="memoWriteTime"></span>
+          <div id="btnBox">
+            <a id="editMemo">編集</a>
+            <div id="deleteMemo">削除</div>
+            <div id="closeMemo">閉じる</div>
+          </div><!-- /#btnBox -->
+        </div><!-- /#btnBox -->
+      
+        <div id="modalMemoContent">
+      
+        </div><!-- /#modalMemoContent -->
+      </div><!-- /div#modalMemoWindow -->
+    </div>
+  `;
+
+// 
+// ここまで　グローバルスコープ　
+// 
+
+// -----------------------------------------------
