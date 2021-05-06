114399beff619208bb143a110a5977eb2bdb802c
korisu07 <Hallo.Dreamy@gmail.com>
2021-05-06 14:58:45 +0900

    Create function deleteConfirm.

--- public/js/action/deleteMemo.js
+++ public/js/action/deleteMemo.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // 削除ボタンを押した際の処理をここに記述します。
+
+// 削除する際の確認画面を出力する処理
+function deleteConfirm( memoNumber ){
+
+  const confirmModal = `
+    <div id="deleteConfirm">
+      <div class="confirmText">削除しますか？</div>
+
+      <form method="post" id="postDelete" action="/delete/${memoNumber}">
+        <button>はい</button>
+      </form>
+
+      <div id="noDel">いいえ</div>
+    </div>
+  `;
+
+  return confirmModal;
+}
