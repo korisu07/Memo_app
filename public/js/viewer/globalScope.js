ac70d82165752c6cf356afdcfc47a307ff7a3e52
korisu07 <Hallo.Dreamy@gmail.com>
2021-05-07 19:15:37 +0900

    First commit. This is file for global scope only.

---
+++ public/js/viewer/globalScope.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// 
+// ★グローバルスコープ
+// 
+
+// firstSetting.js ここから
+
+const 
+  wrapElem = document.querySelector('div.memoWrap'),
+  //メモの数をカウント
+  childCount = wrapElem.childElementCount;
+
+
+// ここまで firstSetting.js
+
+// -----------------------------------------------
+
+// omitContent.js ここから
+
+
+// メモ部分をすべて読み込み
+const 
+  contents = document.querySelectorAll('div.smallMemoContent');
+  
+
+// ここまで omitContent.js
